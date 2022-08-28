c91bab49b0f83ddad020bd2c6c3b6d7750bd38e6
Joel Olutoye <donronald27@gmail.com>
2022-08-28 16:07:26 +0100

    SRC Edited

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "antd/dist/antd.min.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Dashboard } from './view/dashboard'
 import { ErrorPage } from './view/error';
+import { Transactions } from './view/transactions/components/transactions';
 
 export const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/Transactions' element={<Transactions />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
     </Router>
